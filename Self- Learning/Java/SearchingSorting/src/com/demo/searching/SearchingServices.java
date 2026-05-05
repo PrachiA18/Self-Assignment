98356ed6ca92873680e9728f1bb686837c5aee8a
PrachiA18 <andureprachi11@gmail.com>
2026-05-05 17:02:12 +0530

    Added Self Learning Assignments

--- Self- Learning/Java/SearchingSorting/src/com/demo/searching/SearchingServices.java
+++ Self- Learning/Java/SearchingSorting/src/com/demo/searching/SearchingServices.java
@@ -1,5 +1,43 @@
 package com.demo.searching;
 
 public class SearchingServices {
-
+  
+	
+	public static int sequentialSearch(int []arr,int searchnum) {
+		
+		for(int i=0 ;i< arr.length;i++) {
+			if (searchnum== arr[i]) {
+				return i;
+			}
+		}
+		return-1;
+	}
+	
+	public static int binarySearchNonRecursive(int[]arr,int searchnum) {
+		  int low=0;
+		  int high =arr.length-1;
+		  int cnt =0;
+		  while(low<=high) {
+			  int mid=(low+high)/2;
+			  
+			  System.out.println("number of comparisons:"+cnt);
+			  return-1;
+		  }
+		  public static int binarySearchRecursive(int[]arr,int searchnum,int low,int high) {
+			  System.out.println("binary search called low:"+low+ "High"+high);
+			  if(low<=high) {
+				  int mid =(low+high)/2;
+				  if(arr[mid]==searchnum) {
+					  return mid;
+				  }
+				  else if(searchnum<arr[mid]) {
+					  return binarySearchRecursive(arr,searchnum,low,mid-1);
+				  
+				  }else {
+					  return binarySearchRecursive(arr,searchnum,mid+1,high);
+				  }
+			  }
+			 return -1; 
+		  }
+	}
 }
--- Self- Learning/Java/SearchingSorting/src/com/demo/sorting/BubbleSortAlgorithm.java
+++ Self- Learning/Java/SearchingSorting/src/com/demo/sorting/BubbleSortAlgorithm.java
@@ -1,5 +1,70 @@
 package com.demo.sorting;
 
-public class BubbleSortAlgorithm {
 
-}
+import java.util.Arrays;
+
+public class BubbleSortAlgorithm {
+	public static void bubblesort(int[]arr) {
+		for(int i=0 ;i<arr.length;i++) {
+			int count=0;
+			for(int j=i ;j<arr.length;j++) {
+				if(arr[j-i]>arr[j]) {
+					 count++;
+					 int temp=arr[j-i];
+					 arr[j-i]=arr[j];
+					 arr[j]=temp;
+				}
+			}
+			System.out.println("iteration :" +i+"swap count:"+count);
+			System.out.println(Arrays.toString(arr));
+		
+				
+			}
+		}
+		public static void bubbleSortDescending(int[]arr) {
+			 for(int i=0;i<arr.length;i++) {
+				 int count =0;
+				 boolean flag=false;
+				 for(int j=i ;j<arr.length-i;j++) {
+					 if( arr[j-i]<arr[j]) {
+						 count++;
+						 int temp  = arr[j-i];
+						 arr[j-i]= arr[j];
+						 arr[j]=temp;
+					 }
+					 
+				 }
+				 System.out.println("iteration : "+i+" swap count: "+count);
+        		 System.out.println(Arrays.toString(arr));
+
+			 }
+		}
+		 public static void improvedbubbleSort(int[] arr) {
+             
+        	 for(int i=0;i<arr.length;i++) {
+        		 int count=0;
+        		 boolean flag=false;
+        		 for(int j=1;j<arr.length-i;j++) {
+        			 if(arr[j-1]>arr[j]) {
+        				 //swap elements
+        				 count++;
+        				 flag=true;
+        				 int temp=arr[j-1];
+        				 arr[j-1]=arr[j];
+        				 arr[j]=temp;
+        			 }
+        			 
+        		 }
+        		 System.out.println("iteration : "+i+" swap count: "+count);
+        		 System.out.println(Arrays.toString(arr));
+        		 if(!flag) {   
+        			 break;
+        		 }
+        	 }
+		 }
+	}
+
+
+	
+
+
